4a613b39e9aa2383426beb30cdbbfd0bb7cc0d2d
Sergio <sergiopico95@gmail.com>
2023-10-10 14:43:04 +0200

    prueba

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import './App.css';
+import Boton from './Componentes/Boton'
+import Pantalla from './Componentes/Pantalla'
+import BotonClear from './Componentes/BotonClear'
+import { useState } from 'react';
+import React from 'react';
+
+function App() {
+
+  const [input, setInput] = useState('0');
+
+  const agregarInput = val => {
+    if (input ==='0') {setInput(val)} else {setInput(input + val);}}
+
+  const calcularResultado = () => {
+    var resultado = eval(input);
+    setInput (resultado)
+  };
+
+  const manejarClear = () => {
+    setInput ('0')
+  };
+
+  return (
+    <div className='App'>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input}/>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
+        </div>
+      </div>
+    </div>
+  );
+  }
+
+
+export default App;
